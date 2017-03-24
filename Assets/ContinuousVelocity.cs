7f1d30c4e617bb00694b34f9b704c97c9d64f4bf
fcpau <fcpau4@gmail.com>
2017-03-24 19:17:46 +0100

    First Commit

---
+++ Assets/ContinuousVelocity.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ContinuousVelocity : MonoBehaviour {
+
+	public Vector2 velocity;
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		GetComponent<Rigidbody2D>().velocity = velocity;
+	}
+}
---
+++ Assets/Spawn.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawn : MonoBehaviour {
+
+	public GameObject ship;
+	public float interval = 1;
+
+	// Use this for initialization
+	void Start () {
+		InvokeRepeating("SpawnNext", interval, interval);
+	}
+	
+	void SpawnNext () {
+		Instantiate(ship, transform.position, Quaternion.identity);
+	}
+}
