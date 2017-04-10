9e4e9bbbc9edbca2f99ac8585a367d56970abdc8
Pau <fcpau4@gmail.com>
2017-04-10 20:05:18 +0200

    Lives counter and Restart done

--- Assets/BulletDamage.cs
+++ Assets/BulletDamage.cs
@@ -4,22 +4,8 @@ using UnityEngine;
 
 public class BulletDamage : MonoBehaviour {
 
-	public GameObject heart;
-	public GameObject heart2;
-	public GameObject heart3;
-
-	public int counter=3;
-	
-	void OnCollisionEnter2D(Collision2D coll){
-
-		if(coll.gameObject.tag == "ship"){
-			
-			counter--;
-			Destroy(coll.gameObject);
-			
-		}
-		Destroy(gameObject);
-		
-	}
-
+	void OnCollisionEnter2D(Collision2D col)
+    {
+        Destroy(gameObject);
+    }
 }
---
+++ Assets/HealthManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class HealthManager : MonoBehaviour {
+
+    public GameObject[] hearts;
+    private int health;
+
+	// Use this for initialization
+	void Start () {
+        health = hearts.Length;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if(col.gameObject.tag == "enemybull")
+        {
+            health = health - 1;
+            hearts[health].SetActive(false);
+
+            if (health == 0)
+            {
+                Destroy(gameObject);
+                Application.LoadLevel(0);
+            }
+        }
+    }
+   
+}
